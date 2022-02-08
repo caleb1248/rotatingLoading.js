b4f7739a40210e57ae980b447a0cfee9d2115b37
programmer256 <>
2022-02-08 19:45:46 +0000

    addded the api

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const square = document.querySelector("div");
-const buttons = document.querySelectorAll("button");
-buttons[0].onclick = () => square.style.animationPlayState='paused'
-buttons[1].onclick = () => square.style.animationPlayState='running'
+function load(element, speed, condition){
+	var style = document.body.appendChild(document.createElement('style'));
+	style.type='text/css';
+	style.innerHTML=`
+@keyframes rloading{
+	from{
+		transform:rotate(0deg)
+	}
+	to{
+		transform:rotate(360deg)
+	}
+}`
+	element.style.animation = `rloading ${speed}s infinite linear`;
+	return new Promise((resolve, reject) => {
+		var interval = setInterval(function(){
+			if(condition()){
+				style.remove();
+				clearInterval(interval);
+				resolve();
+			}
+		},500)
+	});;
+}
